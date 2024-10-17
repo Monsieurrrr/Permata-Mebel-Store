9024fa0a62e139c21788cb077377d8226152ab6b
Monsieurrrr <ronnorbert29@gmail.com>
2024-10-17 13:35:33 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import Home from './pages/Home'
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
 // import Navbar from './components/Navbar'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>
-}
+
 
 ])
 
 export default function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <Home/>
   )
 }
 
